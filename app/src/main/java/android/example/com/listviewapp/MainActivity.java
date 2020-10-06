286064d9264c60fb11c979c6e8e0dbc645d7a686
Anita Wardani <anitawardani1234@gmail.com>
2020-10-06 21:06:22 +0700

    tugas 3 pertemuan 7

---
+++ app/src/main/java/android/example/com/listviewapp/MainActivity.java
@@ -0,0 +1,40 @@
+package android.example.com.listviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private String[] namanegara=new String[]{
+            "Indonesia", "Malaysia", "Singapore", "Inggris", "Italia", "Belanda"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.list_view);
+        getSupportActionBar().setTitle("ListView Sederhana");
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,android.R.id.text1,namanegara);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Anda Memilih :"+namanegara[i],
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
